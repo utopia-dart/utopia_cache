dc04d57d5270911ee5b9c644f223da3f611676d0
Damodar Lohani <dlohani48@gmail.com>
2022-08-06 13:52:17 +0000

    first implementation

---
+++ example/utopia_cache_example.dart
@@ -0,0 +1,6 @@
+import 'package:utopia_cache/utopia_cache.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/utopia_cache.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library utopia_cache;
+
+export 'src/cache.dart';
+export 'src/adapter.dart';
+export 'src/adapter/filesystem.dart';
+
